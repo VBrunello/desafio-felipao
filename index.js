ed7e50bcc4aa9ecb592e4fcf4f6ae36fe15ad392
Vítor G. Brunello <vitorg.brunello@gmail.com>
2024-04-28 12:48:00 -0300

    Finalizado o desafio

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-let nome = "seu nome";
-let xp = 0;
-let elos = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
-let elo = "";
+let nome = "Nome do herói";
+let xp = 5005;
+let elo = ""
 
 if (xp >= 10001){
     elo = "Radiante";
 }
+else if(xp >= 9001){
+  elo = "Imortal"
+}
+else if(xp >= 8001){
+  elo = "Ascendente"
+}
+else if(xp >= 7001){
+  elo = "Platina"
+}
+else if(xp >= 5001){
+  elo = "Ouro"
+}
+else if(xp >= 2001){
+  elo = "Prata"
+}
+else if(xp >= 1001){
+  elo = "Bronze"
+}
+else{
+  elo = "Ferro"
+}
 
-console.log(nome);
+console.log("Seu herói chamado " + nome + " possui " + xp + " pontos de experiênca e está no elo " + elo + "!")
